be1a27936d89453a7746aa60b3a9dfb09bcca860
TheKevinHub <kevin.accetta95@gmail.com>
2023-11-03 15:03:04 -0400

    Finalize UI and functionality

--- app.js
+++ app.js
@@ -6,59 +6,28 @@ const createRandomRGB = () => {
     return { R, G, B }
 }
 
-let sizeOfGrid = 10;
-
-let container = document.querySelector('.containerGrid');
 let resetBtn = document.querySelector('.btn-reset');
 
-// let createGrid = (amntGrid) => {
-//     let wrapper = document.createElement('div')
-//     wrapper.classList.add('wrapper')
-//     for (let i = 0; i < amntGrid; i++) {
-//         let row = document.createElement('div')
-//         row.classList.add('grid-row')
 
-//         for (let j = 0; j < amntGrid; j++) {
-//             const {R, G, B} = createRandomRGB()
-//             let widthAndHeight = 480/ sizeOfGrid
-//             let gridBox = document.createElement('div')
-//             gridBox.classList.add('grid-box')
-//             gridBox.style.width = `${widthAndHeight}px`
-//             gridBox.style.height = `${widthAndHeight}px`
-//             // hover effects
-//             gridBox.addEventListener('mouseenter', () => {
-//                 const rgbColor = "rgb(" + R + "," + G + "," + B + ")";
-//                 const currentOpacity = gridBox.style.opacity
+let color = 'black'
+let click = false;
 
-//                 // 3 different styles of the Sketch. B/W, Rainbow, Opacity
-//                 // gridBox.style.backgroundColor = 'black'
-//                 gridBox.style.backgroundColor = rgbColor
-//                 // gridBox.style.opacity = Number(currentOpacity) + .1
+document.addEventListener("DOMContentLoaded", function() {
+    createBoard(16)
 
-//                 // gridBox.style.transition = '0.3s'
-//             })
-//             row.appendChild(gridBox);
-//         }
-
-//         wrapper.appendChild(row)
-//     }
-//     container.appendChild(wrapper)
-// }
-
-// createGrid(sizeOfGrid)
-
-// resetBtn.addEventListener('click', () => {
-//     let userSize = Number(prompt('How big do you want the grid to be?'))
-
-//     while (userSize > 100) {
-//         userSize = Number(prompt('Pick a number between 1 and 100.'))
-//     }
-
-//     let wrapper = document.querySelector('.wrapper')
-//     wrapper.remove()
-//     createGrid(userSize)
-// })
-
+    document.querySelector('body').addEventListener("click", function(e) {
+        if(e.target.tagName != "BUTTON") {
+            click = !click
+            let draw = document.querySelector('.draw')
+            if(click) {
+                draw.textContent = "Drawing is ON"
+            }
+            else {
+                draw.textContent = "Drawing is OFF"
+            }
+        }
+    })
+})
 
 let createBoard = (size) => {
     let board = document.querySelector('.containerGrid')
@@ -70,10 +39,50 @@ let createBoard = (size) => {
 
     for (let i = 0; i < numDivs; i++) {
         let div = document.createElement('div')
-        div.style.backgroundColor = 'white'
-        div.style.border = '2px solid black'
+        div.classList.add('divBox')
+        div.addEventListener("mouseover", colorDiv)
         board.appendChild(div)
     }
 }
 
-createBoard(10);
+createBoard(16);
+
+resetBtn.addEventListener('click', () => {
+    let input = Number(prompt('How big do you want the grid to be?'))
+
+    while (input > 100 || input == "") {
+        input = Number(prompt('Pick a number between 1 and 100.'))
+    }
+    createBoard(input)
+    let gridSizeDiv = document.querySelector('.currentGridSize')
+    gridSizeDiv.textContent = `Current grid size: ${input}x${input}`
+})
+
+function colorDiv() {
+    if (click) {
+        if (color == "random") {
+            const {R, G, B} = createRandomRGB()
+            const rgbColor = "rgb(" + R + "," + G + "," + B + ")";
+            this.style.backgroundColor = rgbColor
+            this.style.transition = '0.2s'
+        }
+        else if (color == "black") {
+            this.style.backgroundColor = "black"
+            this.style.transition = '0.2s'
+        }
+        else if (color == "shading"){
+            const currentOpacity = this.style.opacity
+            this.style.opacity = Number(currentOpacity) + .1
+            this.style.backgroundColor = "black"
+        }
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice
+}
+
+function resetBoard() {
+    let divs = document.querySelectorAll('.divBox')
+    divs.forEach((div) => div.style.background = "white")
+}
