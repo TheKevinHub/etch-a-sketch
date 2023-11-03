b24881a1ebf1ec160093bc19561a922c65d73d98
TheKevinHub <kevin.accetta95@gmail.com>
2023-11-03 11:41:49 -0400

    Modify UI & Add buttons

--- app.js
+++ app.js
@@ -1,29 +1,62 @@
-let sizeOfGrid = 30;
-let resetBtn = document.querySelector('button');
+const createRandomRGB = () => {
+    const R = Math.floor(Math.random() * 256)
+    const G = Math.floor(Math.random() * 256)
+    const B = Math.floor(Math.random() * 256)
+
+    return { R, G, B }
+}
+
+let sizeOfGrid = 10;
 
 let container = document.querySelector('.containerGrid');
+let resetBtn = document.querySelector('.btn-reset');
 
 let createGrid = (amntGrid) => {
+    let wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
     for (let i = 0; i < amntGrid; i++) {
         let row = document.createElement('div')
         row.classList.add('grid-row')
 
         for (let j = 0; j < amntGrid; j++) {
-            let widthAndHeight = 960/ sizeOfGrid
+            const {R, G, B} = createRandomRGB()
+            let widthAndHeight = 480/ sizeOfGrid
             let gridBox = document.createElement('div')
             gridBox.classList.add('grid-box')
             gridBox.style.width = `${widthAndHeight}px`
             gridBox.style.height = `${widthAndHeight}px`
             // hover effects
             gridBox.addEventListener('mouseenter', () => {
-                gridBox.style.backgroundColor = 'black'
-                gridBox.style.transition = '0.3s'
+                const rgbColor = "rgb(" + R + "," + G + "," + B + ")";
+                const currentOpacity = gridBox.style.opacity
+
+                // 3 different styles of the Sketch. B/W, Rainbow, Opacity
+                // gridBox.style.backgroundColor = 'black'
+                gridBox.style.backgroundColor = rgbColor
+                gridBox.style.opacity = Number(currentOpacity) + .1
+
+                // gridBox.style.transition = '0.3s'
             })
             row.appendChild(gridBox);
         }
 
-        container.appendChild(row)
+        wrapper.appendChild(row)
     }
+    container.appendChild(wrapper)
 }
 
 createGrid(sizeOfGrid)
+
+resetBtn.addEventListener('click', () => {
+    let userSize = Number(prompt('How big do you want the grid to be?'))
+
+    while (userSize > 100) {
+        userSize = Number(prompt('Pick a number between 1 and 100.'))
+    }
+
+    let wrapper = document.querySelector('.wrapper')
+    wrapper.remove()
+    createGrid(userSize)
+})
+
+// https://www.youtube.com/watch?v=0YjwdFT-O9Y
